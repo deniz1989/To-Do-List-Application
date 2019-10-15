d4be872242944c1a13779363b085c3a36927bd2a
deniz1989 <56403447+deniz1989@users.noreply.github.com>
2019-10-15 12:28:15 +0300

    Last Commit

---
+++ ToDoList/WebAPI/Controllers/ToDoItemsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private IToDoItemService _toDoItemService;
+
+        public ToDoItemsController(IToDoItemService toDoItemService)
+        {
+            _toDoItemService = toDoItemService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(ToDoItem toDoItem)
+        {
+            var result = _toDoItemService.Add(toDoItem);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(ToDoItem toDoItem)
+        {
+            var result = _toDoItemService.Add(toDoItem);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getlistbytodolistid")]
+        public IActionResult GetListByToDoListId(int toDoListId)
+        {
+            var result = _toDoItemService.GetListByToDoListId(toDoListId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+    }
+}
---
+++ ToDoList/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(User user)
+        {
+            var result = _userService.Add(user);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
